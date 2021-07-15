8c4afb3fe5e9c1cce954a291e1a462b217359500
佐藤拓真 <takuma1229tennis@gmail.com>
2021-07-16 03:05:41 +0900

    add logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,22 +6,31 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     get sessions_path
     post sessions_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", sessions_path, count: 0
-    #assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", session_path(@user)
+    delete session_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", sessions_path
+    assert_select "a[href=?]", session_path(@user),      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 
   test "login with valid email/invalid password" do
     get sessions_path
     assert_template 'sessions/index'
     post sessions_path, params: { session: { email:    @user.email,
-                                          password: "invalid" } }
+                              password: "invalid" } }
+    assert_not is_logged_in?
     assert_template 'sessions/index'
     assert_not flash.empty?
     get root_path
