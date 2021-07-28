edaf3d53a4174474524209e3ee502b102abd84ed
佐藤拓真 <takuma1229tennis@gmail.com>
2021-07-28 09:19:32 +0900

    add micropost image setting

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,6 +4,7 @@ class MicropostsController < ApplicationController
   def create
     @user = User.find_by(id: current_user.id)
     @micropost = current_user.microposts.build(micropost_params)
+    @micropost.image.attach(params[:micropost][:image])
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to @user
@@ -24,7 +25,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content)
+      params.require(:micropost).permit(:content, :image)
     end
 
     def correct_user
