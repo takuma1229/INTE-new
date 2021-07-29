264f6f1de1e0440996c297c47c48d9a9e2fbbe41
佐藤拓真 <takuma1229tennis@gmail.com>
2021-07-29 09:10:06 +0900

    almost finish dm-index

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,12 +9,24 @@ class RoomsController < ApplicationController
 
   def show
     @room = Room.find(params[:id])
+    @companion = User.find(params[:id])
     if Entry.where(:user_id => current_user.id, :room_id => @room.id).present?
-      @messages = @room.messages.all
+      @messages = @room.messages.order(created_at: :desc)
       @message = Message.new
       @entries = @room.entries
     else
       redirect_back(fallback_location: root_path)
     end
+  end
+
+  def index
+    @rooms = Room.all
+    @user = current_user
+    @currentEntries = current_user.entries
+    myRoomIds = []
+    @currentEntries.each do | entry |
+      myRoomIds << entry.room.id
+    end
+    @anotherEntries = Entry.where(room_id: myRoomIds).where('user_id != ?', @user.id)
   end
 end
