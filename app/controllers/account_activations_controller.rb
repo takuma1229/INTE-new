507931bcf9c2e0c58561f605ed76e013cd938282
佐藤拓真 <takuma1229tennis@gmail.com>
2021-07-30 23:47:18 +0900

    modify after account activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -11,7 +11,7 @@ class AccountActivationsController < ApplicationController
       user.activate
       log_in user
       flash[:success] = "Account activated!"
-      redirect_to detail_user_url
+      redirect_to detail_user_url(user)
     else
       flash[:danger] = "Invalid activation link"
       redirect_to root_url
