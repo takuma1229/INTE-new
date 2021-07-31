c5b3141bce7ff0b80033f2caf604ed128d160e4b
佐藤拓真 <takuma1229tennis@gmail.com>
2021-07-31 14:09:39 +0900

    remove likes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_191055) do
+ActiveRecord::Schema.define(version: 2021_07_31_050810) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
