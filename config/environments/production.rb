2cda5a5916f123c72513705c22950a75c22c7369
佐藤拓真 <takuma1229tennis@gmail.com>
2021-08-04 00:31:23 +0900

    modify sendgrid setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  host = '【Herokuのサブドメイン】.herokuapp.com'
+  host = 'protected-island-35085.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
      :port => 587,
