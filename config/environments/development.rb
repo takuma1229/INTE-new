3ff24c178a436bb82551e9a843a57b9328806949
佐藤拓真 <takuma1229tennis@gmail.com>
2021-08-04 23:52:52 +0900

    introduce action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,21 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Rails.application.configure do
+    #--- 中略 ---#
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      port:                 587,
+      address:              'smtp.gmail.com',
+      domain:               'gmail.com',
+      user_name:            'inteofficial@gmail.com',
+      password:             'otukare48',
+      authentication:       'login',
+      enable_starttls_auto: true
+    }
+  end
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
