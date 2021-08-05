24fcbeef6c014b18a172dac64875cb653aba79cc
佐藤拓真 <takuma1229tennis@gmail.com>
2021-08-05 11:24:02 +0900

    modify google app password into row

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
       address:              'smtp.gmail.com',
       domain:               'gmail.com',
       user_name:            'inteofficial@gmail.com',
-      password:             Rails.application.credentials.google[:GOOGLE_APP_PASSWORD],
+      password:             'xdasewkpicroufvm',
       authentication:       'login',
       enable_starttls_auto: true
     }
